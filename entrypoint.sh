95d7a65880408d03c01bd0322098ed5e240f9c3a
Forest10 <github.forest10@gmail.com>
2019-12-15 16:29:33 +0800

    加入本地七牛cache实现增量更新上传(外加多线程)

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,11 @@ if [ -n "${EMAIL}" ]; then
 else
     PUBLISH_EMAIL="github.forest10@gmail.com"
 fi
+if [ -n "${QSHELL_HOME}" ]; then
+    ACTION_QSHELL_HOME=${QSHELL_HOME}
+else
+    ACTION_QSHELL_HOME="/home/runner/.qshell"
+fi
 
 if [ -n "${PUBLISH_REPOSITORY}" ]; then
     PRO_REPOSITORY=${PUBLISH_REPOSITORY}
@@ -91,6 +96,7 @@ echo "del .git ok!"
 PUBLIC_DIR_PATH=`pwd`
 echo "退回上一层目录!"
 cd ../
+QSHELL_DIR_PATH=`pwd`
 echo "Start setup qshell!"
 wget http://devtools.qiniu.com/qshell-linux-x64-v2.4.0.zip -O qshell.zip
 unzip qshell.zip
@@ -98,12 +104,31 @@ mv qshell-linux-x64-v2.4.0 qshell
 chmod u+x qshell
 echo "setup qshell done!"
 
+echo "退回上一层目录!"
+
+cd $ACTION_QSHELL_HOME
+echo 'Start get qshell cache from git'
+git clone https://$PERSONAL_TOKEN@github.com/${QINIU_LOCAL_CACHE_GIT_REPOSITORY}.git
+git checkout ${QINIU_LOCAL_CACHE_GIT_REPOSITORY_BRANCH}
+git pull
+
+cd ${QSHELL_DIR_PATH}
 echo 'Start run qshell account'
 ./qshell account ${QINIU_AK} ${QINIU_SK} ${QINIU_USER_NAME}
 echo 'Start run qshell upload2'
 ##增量更新上传(外加多线程)
 ./qshell qupload2  --overwrite --src-dir=${PUBLIC_DIR_PATH}/ --bucket=${QINIU_BUCKET}  --rescan-local --thread-count 16
 echo 'done  upload qiniu'
+echo 'qiniu upload2 cache to git'
+##加入不报错 就把当前的变化直接传送到git上
+cd $ACTION_QSHELL_HOME
+git add .
+git commit -m "transfer local upload2 cache to git"
+git push
+echo 'qiniu upload2 cache to git done!'
+
+
+
 
 
 
