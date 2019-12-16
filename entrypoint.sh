a96d90cba9d805ff598d7157d715c9b19f56134a
Forest10 <github.forest10@gmail.com>
2019-12-16 18:42:51 +0800

    git diff HEAD  HEAD~1 --name-only

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,16 +44,16 @@ echo "Deploy to ${PRO_REPOSITORY}"
 
 # Directs the action to the the Github workspace.
 cd $GITHUB_WORKSPACE
-
-echo "npm install ..."
-npm install
-
-
-echo "Clean folder ..."
-./node_modules/hexo/bin/hexo clean
-
-echo "Generate file ..."
-./node_modules/hexo/bin/hexo generate
+#
+#echo "npm install ..."
+#npm install
+#
+#
+#echo "Clean folder ..."
+#./node_modules/hexo/bin/hexo clean
+#
+#echo "Generate file ..."
+#./node_modules/hexo/bin/hexo generate
 
 cd $PUBLISH_DIR
 
