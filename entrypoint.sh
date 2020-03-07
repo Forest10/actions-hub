6a0f6dabae7170e4a26cb0d189bea9ab34c99f2e
Forest10 <github.forest10@gmail.com>
2020-03-07 16:19:47 +0800

    echo 'date' >> today.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,6 +47,7 @@ cp -R  ../githubTmp/* ./
 git config user.name "Forest10"
 git config user.email ${PUBLISH_EMAIL}
 # 进入 other git 开始操作
+echo 'date' >> today.txt
 git add .
 git commit -m "Sync From GitHub By sync-2-gitee action"t
 # push to other GIT
