0aa49646481931b253e109bd952bd645ccba0165
Forest10 <github.forest10@gmail.com>
2020-03-07 15:49:42 +0800

    access

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,7 @@ else
     PUBLISH_EMAIL="github.forest10@gmail.com"
 fi
 git clone https://${GITEE_USERNAME}:${GITEE_TOKEN}@${GITEE_REF}  giteeTmp
+
 git clone https://${PRIVATE_GITHUB_TOKEN}@${GITHUB_REF}  githubTmp
 
 
