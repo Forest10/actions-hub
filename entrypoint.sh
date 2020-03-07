f3a890c5b2634cc2022ff4729827cd4d701617eb
Forest10 <github.forest10@gmail.com>
2020-03-07 16:07:06 +0800

    otherGitTmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,12 @@ set -e
 
 
 # check values
-if [ -z "${GITHUB_REF}" ]; then
-    echo "You must provide the action with GITHUB_REF in order to deploy."
+if [ -z "${GITHUB_REPOSITORY}" ]; then
+    echo "You must provide the action with GITHUB_REPOSITORY in order to deploy."
     exit 1
 fi
-if [ -z "${GITHUB_USERNAME}" ]; then
-    echo "You must provide the action with GITHUB_USERNAME in order to deploy."
-    exit 1
-fi
-if [ -z "${GITEE_REF}" ]; then
-    echo "You must provide the action with GITEE_REF in order to deploy."
+if [ -z "${OTHERGITEE_HTTPS_REF}" ]; then
+    echo "You must provide the action with GITEE_REF in order to deploy.like xxx.com/OTHER_GIT_USERNAME/YYY.git"
     exit 1
 fi
 if [ -z "${PRIVATE_GITHUB_TOKEN}" ]; then
@@ -21,12 +17,12 @@ if [ -z "${PRIVATE_GITHUB_TOKEN}" ]; then
     exit 1
 fi
 
-if [ -z "${GITEE_USERNAME}" ]; then
-    echo "You must provide the action with GITEE_USERNAME in order to deploy."
+if [ -z "${OTHER_GIT_USERNAME}" ]; then
+    echo "You must provide the action with OTHER_GIT_USERNAME in order to deploy."
     exit 1
 fi
-if [ -z "${GITEE_TOKEN}" ]; then
-    echo "You must provide the action with GITEE_TOKEN in order to deploy."
+if [ -z "${OTHER_GIT_TOKEN}" ]; then
+    echo "You must provide the action with OTHER_GIT_TOKEN in order to deploy."
     exit 1
 fi
 
@@ -37,22 +33,21 @@ else
     PUBLISH_EMAIL="github.forest10@gmail.com"
 fi
 git clone https://${PRIVATE_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git githubTmp
-git clone https://${GITEE_USERNAME}:${GITEE_TOKEN}@${GITEE_REF}  giteeTmp
+git clone https://${OTHER_GIT_USERNAME}:${OTHER_GIT_TOKEN}@${GITEE_REF}  otherGitTmp
 
 
 
 cd ./githubTmp
 git pull
-cd ../giteeTmp
+cd ../otherGitTmp
 git pull
-# 把github的文件全量复制到gitee中
+# 把github的文件全量复制到otherGitTmp中
 cp -R  ../githubTmp/* ./
 # 设置用户名mail
 git config user.name "Forest10"
 git config user.email ${PUBLISH_EMAIL}
-echo 'date' > today.txt
-# 进入gitee 开始操作
+# 进入 other git 开始操作
 git add .
 git commit -m "Sync From GitHub By sync-2-gitee action"t
-# push to GITEE
+# push to other GIT
 git push
