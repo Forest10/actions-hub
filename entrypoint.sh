8764af04a7b2f549916739458ff2fc1d6573f95a
Forest10 <github.forest10@gmail.com>
2020-03-07 14:44:06 +0800

    初版.先使用一个分支的

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,46 @@
 #!/bin/sh -l
 
 set -e
-echo 'hello master'
+
+
+# check values
+if [ -n "${GITHUB_REF}" ]; then
+    echo "You must provide the action with GITHUB_REF in order to deploy."
+    exit 1
+fi
+if [ -n "${GITEE_REF}" ]; then
+    echo "You must provide the action with GITEE_REF in order to deploy."
+    exit 1
+fi
+
+if [ -n "${GITEE_USERNAME}" ]; then
+    echo "You must provide the action with GITEE_USERNAME in order to deploy."
+    exit 1
+fi
+if [ -n "${GITEE_TOKEN}" ]; then
+    echo "You must provide the action with GITEE_TOKEN in order to deploy."
+    exit 1
+fi
+
+
+if [ -n "${EMAIL}" ]; then
+    PUBLISH_EMAIL=${EMAIL}
+else
+    PUBLISH_EMAIL="github.forest10@gmail.com"
+fi
+git clone ${GITHUB_REF} githubTmp
+git clone ${GITEE_USERNAME}:${GITEE_TOKEN}@${GITEE_REF}  giteeTmp
+cd ./githubTmp
+git pull
+cd ../giteeTmp
+git pull
+# 把github的文件全量复制到gitee中
+cp -R  ../githubTmp/* ./
+# 设置用户名mail
+git config user.name "Forest10"
+git config user.email "github.forest10@gmail.com"
+# 进入gitee 开始操作
+git add .
+git commit -m "Sync From GitHub By sync-2-gitee action"
+# push to GITEE
+git push
